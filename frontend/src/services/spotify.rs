8642fa9fa57925b66b1984e63a6eff4634e3fa56
grostaco <grostaco@gmail.com>
2022-08-02 10:33:46 +0700

    feat: add static/

--- frontend/src/services/spotify.rs
+++ frontend/src/services/spotify.rs
@@ -3,7 +3,7 @@ use spotify::Tracks;
 use crate::error::Error;
 
 pub async fn get_tracks() -> Result<Tracks, Error> {
-    let res = reqwest::get("https://grostaco.netlify.app/api/spotify/tracks")
+    let res = reqwest::get("https://grostaco-1f57c2.netlify.live/api/spotify/tracks")
         .await
         .map_err(|_| Error::ReqestError)?;
     let tracks: Tracks =
