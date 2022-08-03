04a0d557e09f05eb39aebbb2f9932513b02ed311
grostaco <grostaco@gmail.com>
2022-08-04 00:40:01 +0700

    heroku update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./run.sh
+web: ROCKET_ADDRESS=0.0.0.0 ROCKET_PORT=$PORT ROCKET_KEEP_ALIVE=0 ./target/release/backend
--- run.sh
+++
@@ -1,4 +0,0 @@
-cd frontend
-trunk build --release 
-cd ..
-ROCKET_ADDRESS=0.0.0.0 ROCKET_PORT=$PORT ROCKET_KEEP_ALIVE=0 ./target/release/backend
