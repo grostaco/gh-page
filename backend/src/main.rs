6ac787e03aa2f7c7dad364eb746fee94cfe0ce7d
grostaco <grostaco@gmail.com>
2022-08-04 17:15:45 +0700

    feat: re-enable spotify feature

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,34 @@
 use std::env;
 
 use rocket::{self, fs::FileServer, launch, routes, tokio::sync::Mutex};
-use routes::steam::{get_info, get_owned, get_recent};
+use routes::{
+    spotify::get_tracks,
+    steam::{get_info, get_owned, get_recent},
+};
 use services::spotify::{types::Inner, Spotify};
 
 mod routes;
 
 #[launch]
 async fn rocket() -> _ {
-    // let spotify = match env::var("SPOTIFY") {
-    //     Ok(val) => {
-    //         let inner: Inner = serde_json::from_str(&val).unwrap();
-    //         Spotify::from_inner(inner, "spotify.json").await.unwrap()
-    //     }
-    //     Err(_) => Spotify::new("spotify.json").await.unwrap(),
-    // };
+    let spotify = match env::var("SPOTIFY") {
+        Ok(val) => {
+            let inner: Inner = serde_json::from_str(&val).unwrap();
+            Spotify::from_inner(inner, "spotify.json").await.unwrap()
+        }
+        Err(_) => Spotify::new("spotify.json").await.unwrap(),
+    };
     let steam = services::steam::Client::new(env::var("STEAM").unwrap());
     let steam_id: u64 = env::var("STEAM_ID").unwrap().parse().unwrap();
 
     // TODO: separate the api routes
     rocket::build()
         .mount("/", FileServer::from("target/release/static/"))
-        .mount("/api/", routes![get_owned, get_recent, get_info])
-        // .manage(Mutex::new(spotify))
+        .mount(
+            "/api/",
+            routes![get_owned, get_recent, get_info, get_tracks],
+        )
+        .manage(Mutex::new(spotify))
         .manage(steam)
         .manage(steam_id)
 }
--- backend/src/routes/spotify.rs
+++ backend/src/routes/spotify.rs
@@ -1,10 +1,10 @@
 use rocket::{get, serde::json::Json, tokio::sync::Mutex, State};
 use services::spotify::{types::Tracks, Spotify};
 
-// #[get("/spotify/tracks")]
-// pub async fn get_tracks(spotify: &State<Mutex<Spotify>>) -> Json<Tracks> {
-//     let mut spotify = spotify.lock().await;
-//     spotify.refresh_token().await.unwrap();
-//     let tracks = spotify.tracks().await.unwrap();
-//     Json(tracks)
-// }
+#[get("/spotify/tracks")]
+pub async fn get_tracks(spotify: &State<Mutex<Spotify>>) -> Json<Tracks> {
+    let mut spotify = spotify.lock().await;
+    spotify.refresh_token().await.unwrap();
+    let tracks = spotify.tracks().await.unwrap();
+    Json(tracks)
+}
